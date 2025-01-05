c22ff0e5e45d4c2990db285954b14d286cf82010
Alek V <alekvasek@icloud.com>
2025-01-05 16:41:47 -0600

    Updated Code

--- web_ui/app.py
+++ web_ui/app.py
@@ -1,12 +1,98 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
+import pandas as pd
 
 
 app = Flask(__name__)
 
+workspaces = pd.DataFrame(columns=['name', 'description', 'date', 'sources', 'id', 'icon', 'ai_description'])
+
+demo_workspace = pd.DataFrame(data={'name': 'Workspace Name', 'description': 'Workspace Description', 'date': 'Dec 16, 2024', 'sources': 14, 'id': -1, 'icon': '&#128373;', 'ai_description': "The provided texts offer a multifaceted view of Cold War intelligence. One memoir recounts a CIA officer's experiences in Vietnam, the Philippines, Cambodia, and Iran, detailing his operational work and reflections on US foreign policy. Other documents are declassified CIA records, including analyses of the Soviet Union, the Korean War, and the Cuban Missile Crisis, revealing internal debates, assessments of Soviet military capabilities, and the crucial role of satellite imagery. Finally, a monograph explores the counterintelligence literature of the period, examining controversies surrounding Soviet penetration and the impact of key defectors' testimonies."}, index=[0])
+
+workspaces = pd.concat([demo_workspace, workspaces], ignore_index=True)
+
+
+id_number = 0
+
 @app.route('/')
-def control():
-	return render_template("index.html")
-  
+def main():
+
+	return render_template("main.html", workspaces=workspaces.iterrows())
+
+@app.route('/createWorkspace', methods=['POST'])
+def createWorkspace():
+	global workspaces
+	global id_number
+	
+	name = request.form['name']
+	description = request.form['description']
+
+	current_date = datetime.datetime.now()
+	date = f"{current_date.strftime("%b")} {current_date.day}, {current_date.year}"
+	
+	sources = request.form['sources']
+
+	id_number += 1
+
+	new_workspace = pd.DataFrame({'name': name, 'description': description, 'date': date, 'sources': sources, 'id': int(float(id_number)), 'icon': '', 'ai_description': ''}, index=[0])
+	workspaces = pd.concat([new_workspace, workspaces], ignore_index=True)
+	return redirect(url_for('main'))
+
+@app.route('/deleteAllWorkspaces', methods=['POST'])
+def deleteAllWorkspaces():
+	global workspaces
+	workspaces = pd.DataFrame(columns=['name', 'description', 'date', 'sources', 'id', 'icon', 'ai_description'])
+	
+	workspaces = pd.concat([demo_workspace, workspaces], ignore_index=True)
+
+	
+	
+	return redirect(url_for('main'))
+
+
+@app.route('/deleteWorkspace', methods=['POST'])
+def deleteWorkspace():
+	global workspaces
+	id = int(float(request.form['workspace_id']))
+	print(id)
+	
+
+	if id == -1:
+		print("Deleting Demo Card")
+
+		index = 0
+		workspaces = workspaces.drop(index)
+		
+		return redirect(url_for('main'))
+	
+	elif workspaces.index[workspaces['id'] == id].tolist()[0] > 1:
+		print("Error Multiple Workspaces with same ID")
+
+		return redirect(url_for('main'))
+	else:
+
+		
+		index = workspaces.index[workspaces['id'] == int(id)].tolist()[0]
+		workspaces = workspaces.drop(index)
+		return redirect(url_for('main'))
+	
+
+@app.route('/workspace/<workspace_id>', methods=['GET'])
+def workspace(workspace_id):
+	global workspaces
+
+	print("Workspace ID: ")
+
+	print("Workspaces: ")
+	print(workspaces)
+
+	workspace = workspaces[workspaces["id"] == int(float(workspace_id))]
+
+	print("Workspace: ")
+	print(workspace)
+	return render_template("workspace_editor.html", workspace=workspace)
+
 if __name__ == "__main__":
+	workspaces = demo_workspace(workspaces)
 	app.run(host="0.0.0.0", port=8080, debug=True)
     
---
+++ web_ui/static/js/script.js
@@ -0,0 +1,138 @@
+// Select the button
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// Get the user's theme preference from local storage, if it's available
+const currentTheme = localStorage.getItem("theme");
+// If the user's preference in localStorage is dark...
+if (currentTheme == "dark") {
+  // ...let's toggle the .dark-theme class on the body
+  if (!document.body.classList.contains("dark-theme")) {
+    document.body.classList.add("dark-theme");
+  }
+// Otherwise, if the user's preference in localStorage is light...
+} else if (currentTheme == "light") {
+  // ...let's toggle the .light-theme class on the body
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+  }
+} else {
+    // If the user's preference is not in localStorage, default to light mode
+    // Fallback to OS preference
+    if (prefersDarkScheme.matches) {
+        document.body.classList.add("dark-theme");
+    } else {
+        document.body.classList.remove("dark-theme");
+    }
+}
+
+// Listen for a click on the button 
+btn.addEventListener("click", function () {
+  // Toggle between light and dark mode
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.remove("dark-theme");
+    currentTheme = "light";
+  } else {
+    document.body.classList.add("dark-theme");
+    currentTheme = "dark";
+  }
+
+  // Save the current theme to local storage
+  localStorage.setItem("theme", currentTheme);
+});
+
+// Get the modal
+var modal = document.getElementById("workspace_creator_model");
+
+// Get the button that opens the modal
+var modalBtn = document.getElementById("modelBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var workspaceCardButtons = document.querySelectorAll(".option_button");
+var workspaceDropdowns = document.querySelectorAll(".workspace_options_content");
+
+
+
+workspaceCardButtons.forEach((button, index) => {
+  button.onclick = function(event) {
+    event.stopPropagation(); // Prevent the event from bubbling up
+   
+    workspaceDropdowns.forEach(dropdown => dropdown.classList.remove("show"));
+
+    workspaceDropdowns[index].classList.toggle("show");
+
+  }
+});
+
+window.onclick = function(event) {
+  if (![...workspaceCardButtons].includes(event.target)) {
+    workspaceDropdowns.forEach(dropdown => dropdown.classList.remove("show"));
+  }
+}
+
+
+const deleteCardButtons = document.querySelectorAll(".delete_card_button");
+
+deleteCardButtons.forEach((button, index) => {
+  button.onclick = function(event) {
+    event.stopPropagation(); // Prevent the event from bubbling up
+
+    var workspaceCards = document.querySelectorAll(".workspace_card");
+
+    cardId = workspaceCards[index].id;
+
+    $.ajax({
+      url: `/deleteWorkspace`,
+
+      type: 'POST',
+      data: {
+        workspace_id: cardId
+      },
+      success: function (response) {
+          console.log(response);
+          location.reload(true);
+
+      },
+      error: function (error) {
+          console.log(error);
+      }
+    });
+  };
+});
+
+$(document).ready(function(){
+  $("button#deleteBtn").click(function(){
+    $.ajax({
+      url: `/deleteAllWorkspaces`,
+
+      type: 'POST',
+      success: function (response) {
+          console.log(response);
+          location.reload(true);
+
+      },
+      error: function (error) {
+          console.log(error);
+      }
+  });
+  });
+});
